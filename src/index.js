419c887a05116124ee90bb1c1ddeaab3e9a19c39
Daniel Leal <dlleal@uc.cl>
2022-03-27 23:01:44 -0300

    Correctly type stuff

--- src/index.js
+++ src/index.js
@@ -1,44 +1,77 @@
 import promptSync from 'prompt-sync';
 
+/**
+ * @typedef {import('./types.js').Player} Player
+ * @typedef {import('./types.js').Play} Play
+ * @typedef {import('./types.js').roundExecutor} roundExecutor
+ */
+
 const prompt = promptSync();
 
+/**
+ * @param {Play} play
+ * @returns {number}
+ */
 const getScoreFromThrow = (play) => {
   if (play === 'DB') {
     return 50;
-  } else if (play === 'SB') {
+  }
+  if (play === 'SB') {
     return 25;
   }
   return play.map((num) => Number(num)).reduce((acc, curr) => acc * curr, 1);
 };
 
-const calculatePoints = (points, plays) => Math.abs(
-  plays.reduce((acc, play) => acc - getScoreFromThrow(play), points),
+/**
+ * @param {number} score
+ * @param {Array<Play>} plays
+ * @returns {number}
+ */
+const calculateScore = (score, plays) => Math.abs(
+  plays.reduce((acc, play) => acc - getScoreFromThrow(play), score),
 );
 
-const calculatePlayerPoints = ({ name, points }, plays) => (
-  { name, points: calculatePoints(points, plays) }
+/**
+ *
+ * @param {Player} player
+ * @param {Array<Play>} plays
+ * @returns {Player}
+ */
+const calculatePlayerScore = ({ name, score }, plays) => (
+  { name, score: calculateScore(score, plays) }
 );
 
-const playRound = f => (players) => {
+/**
+ * @param {roundExecutor} f
+ * @returns {roundExecutor}
+ */
+const playRound = (f) => (players) => {
   const [currentPlayer, ...otherPlayers] = players;
   console.log(`${currentPlayer.name}'s turn`);
   const play = JSON.parse(prompt('Please enter his/her play: '));
-  const currentPlayerMod = calculatePlayerPoints(currentPlayer, play);
-  console.log(`${currentPlayerMod.name} has ${currentPlayerMod.points} left`);
-  if (currentPlayerMod.points !== 0) {
+  const currentPlayerMod = calculatePlayerScore(currentPlayer, play);
+  console.log(`${currentPlayerMod.name} has ${currentPlayerMod.score} left`);
+  if (currentPlayerMod.score !== 0) {
     return f([...otherPlayers, currentPlayerMod]);
   }
   return currentPlayerMod;
 };
 
-const Y = f => (x => x(x))(x => f(y => x(x)(y)));
+const Y = (f) => ((x) => x(x))((x) => f((y) => x(x)(y)));
 
-const initGame = (...names) => names.map((name) => ({ name, points: 501 }));
+/**
+ * @param  {Array<string>} names
+ * @returns {Array<Player>}
+ */
+const initialState = (...names) => names.map((name) => ({ name, score: 501 }));
 
+/**
+ * @param  {Array<string>} names
+ */
 const playGame = (...names) => {
-  const players = initGame(...names);
-  // const winner = playRound(players);
-  const winner = Y(playRound)(players) ;
+  const players = initialState(...names);
+  /** @type {Player} */
+  const winner = Y(playRound)(players);
   console.log(`${winner.name} won`);
 };
 
---
+++ src/types.js
@@ -0,0 +1,17 @@
+/**
+ * @typedef {Object} Player
+ * @property {string} name
+ * @property {number} score
+ */
+
+/**
+ * @typedef {'DB' | 'SB' | [number, number]} Play
+ */
+
+/**
+ * @callback roundExecutor
+ * @param {Array<Player>} players
+ * @returns {Player}
+ */
+
+export default {};
